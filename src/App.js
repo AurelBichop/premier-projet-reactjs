b5b975a6c50924d8b1b03bdfc490af20b107658d
Aurelien <linux.aurelien@gmail.com>
2020-03-16 15:28:21 +0100

    fin des bases de React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Membre from './components/Membre'
+import Button from './components/Button'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const famille = {
+  membre1: {
+    nom: 'Aurel',
+    age: 33
+  },
+  membre2: {
+    nom: 'Raph',
+    age: 32
+  },
+  membre3: {
+    nom: 'Adrien',
+    age: 1
+  },
+  membre4: {
+    nom: 'Aby',
+    age: 9
+  },
+}
+
+
+class App extends Component {
+  state = {
+    famille
+  }
+
+  handleClick = (num) => {
+    const famille = {...this.state.famille }
+    famille.membre1.age += num
+    this.setState({ famille })
+  }
+
+  handleChange = event => {
+    const famille = {...this.state.famille }
+    const nom = event.target.value
+    famille.membre1.nom = nom
+    this.setState({famille})
+  }
+
+  render() {
+    const { titre } = this.props
+    const { famille } = this.state
+    return (
+      <div className="App">
+        <h1>{titre}</h1>
+        <input value={famille.membre1.nom} 
+        onChange={this.handleChange} 
+        type="text" />
+        <Membre
+          age={famille.membre1.age}
+          nom={famille.membre1.nom} />
+        <Membre
+          age={famille.membre2.age}
+          nom={famille.membre2.nom} />
+        <Membre
+          age={famille.membre3.age}
+          nom={famille.membre3.nom} />
+        <Membre
+          age={famille.membre4.age}
+          nom={famille.membre4.nom} >
+          <strong>I am a dog !</strong>
+        </Membre>
+        <Button
+          vieillir={() => this.handleClick(2)}
+        />
+      </div>
+    )
+  }
+
 }
 
 export default App;
+
+
